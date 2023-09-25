bbb6f9e2efae2757c11932eeabf8ebeeda225255
mahdi <mahdi.313.ab@gmail.com>
2023-09-25 20:09:30 +0330

    right panel and markers added to map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
+import { markers } from "@/data/markers";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import mapboxgl from "mapbox-gl";
+import mapboxgl, { LngLatLike } from "mapbox-gl";
 import { FC, useEffect, useRef } from "react";
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g";
@@ -7,7 +8,9 @@ mapboxgl.accessToken =
 export interface MapType {}
 
 const Map: FC<MapType> = () => {
+  const center: LngLatLike = [54.52824840411543, 24.37560045517577];
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const panelRef = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
   const styles = [
     "mapbox://styles/mapbox/streets-v12",
@@ -19,12 +22,11 @@ const Map: FC<MapType> = () => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      center: [51.388973, 35.689198],
+      center,
       style: styles[1], // style URL
       zoom: 5,
       // customAttribution: "MY MAP",
       attributionControl: false,
-      cooperativeGestures: true,
     });
     // Add zoom and rotation controls to the map.
     map.current.addControl(new mapboxgl.NavigationControl(), "top-left");
@@ -47,13 +49,15 @@ const Map: FC<MapType> = () => {
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
+        autocomplete: true,
       }),
       "top-right"
     );
+    // Create a default Marker and add it to the map.
     const marker = new mapboxgl.Marker({
       draggable: true,
     })
-      .setLngLat([51.388973, 35.689198])
+      .setLngLat(center)
       .addTo(map.current);
 
     function onDragEnd() {
@@ -62,11 +66,65 @@ const Map: FC<MapType> = () => {
     }
 
     marker.on("dragend", onDragEnd);
+    // Create a default Marker and add it to the map. End
+    // Add markers to the map.
+    for (const marker of markers.features) {
+      // Create a DOM element for each marker.
+      const el = document.createElement("div");
+      const width = marker.properties.iconSize[0];
+      const height = marker.properties.iconSize[1];
+      el.className = "marker";
+      el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+      el.style.width = `${width}px`;
+      el.style.height = `${height}px`;
+      el.style.backgroundSize = "100%";
+
+      el.addEventListener("click", () => {
+        window.alert(marker.properties.message);
+      });
+
+      new mapboxgl.Marker(el, {
+        draggable: true,
+      })
+        .setLngLat(marker.geometry.coordinates as LngLatLike)
+        .addTo(map.current);
+    }
+    // Add markers to the map. End
   });
+  const toggleSidebar = () => {
+    const panel = panelRef.current;
+    const collapsed = panel?.classList.toggle("translate-x-[262px]");
+
+    map.current?.easeTo({
+      // padding:padding,
+      padding: {
+        right: collapsed ? 0 : 262,
+        left: 0,
+        top: 0,
+        bottom: 0,
+      },
+      duration: 1000, // In ms. This matches the CSS transition duration property.
+    });
+  };
 
   return (
     <>
       <div ref={mapContainerRef} className="map-container"></div>
+      {/* translate-x-[262px] */}
+      <div className=" px-3 pb-3 pt-14">
+        <div
+          ref={panelRef}
+          className="bg-white absolute h-[100vh] w-[262px] right-0 top-0 flex flex-col justify-center items-center transition-all text-gray-400 "
+        >
+          Right Sidebar
+          <div
+            className="px-2 pt-2 pb-1 absolute top-[48.5vh] -left-8 rounded-lg bg-white text-gray-700 cursor-pointer"
+            onClick={toggleSidebar}
+          >
+            &#9776;
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/data/markers.ts
@@ -0,0 +1,38 @@
+export const markers = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        message: "Foo",
+        iconSize: [60, 60],
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [54.53199505863063, 24.32528514388389],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        message: "Bar",
+        iconSize: [50, 50],
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [54.6611076360864, 24.395611325370922],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        message: "Baz",
+        iconSize: [40, 40],
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [54.6435187236014, 24.29566140405926],
+      },
+    },
+  ],
+};
